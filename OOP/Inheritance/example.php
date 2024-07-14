2de1b6980d1fd383b6b72bb108a60ce7cca720fe
Murad Alwan <free1soft@gmail.com>
2024-07-14 17:26:40 +0300

    static data memebers

--- OOP/Inheritance/example.php
+++ OOP/Inheritance/example.php
@@ -1,48 +1,24 @@
 <?php
-interface Chargeable
+class Human
 {
-    public function chargeBattery();
-    public function checkBatteryStatus();
-}
-abstract class ElectronicDevice implements Chargeable
-{
-    protected $batteryLevel;
+    public $name;
+    public $age;
+    public $gender;
+    public $height;
+    public $weight;
+    static public $count = 0;
     public function __construct()
     {
-        $this->batteryLevel = 0; // default battery level
-    }
-    abstract public function chargeBattery();
-    abstract public function checkBatteryStatus();
-}
-
-class Smartphone extends ElectronicDevice
-{
-    public function chargeBattery()
-    {
-        $this->batteryLevel = 100; // Simulate charging to full
-        echo "Smartphone battery is fully charged.\n";
+        self::$count++;
     }
 
-    public function checkBatteryStatus()
+    public function speak()
     {
-        echo "Smartphone battery level is at {$this->batteryLevel}%.\n";
+        return "Hello";
     }
 }
 
-class Laptop extends ElectronicDevice
-{
-    public function chargeBattery()
-    {
-        $this->batteryLevel = 80; // Simulate charging to full
-        echo "Laptop battery is fully charged.\n";
-    }
+$person1 = new Human();
+$person2 = new Human();
 
-    public function checkBatteryStatus()
-    {
-        echo "Laptop battery level is at {$this->batteryLevel}%.\n";
-    }
-}
-
-$smartphone = new Smartphone();
-$smartphone->chargeBattery();
-$smartphone->checkBatteryStatus();
+echo Human::$count;
